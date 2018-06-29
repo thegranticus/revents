0da79d9e3ba4a12d2fd50d0b3db473b0b34a6b11
thegranticus <gc@thegranticus.com>
2018-06-29 12:25:32 -0400

    initalize commit

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { EventDashboard } from '../../features/event/EventDashboard/EventDashboard'
 
 class App extends Component {
   render() {
     return (
-        <div>
-            <h1>BARWIS Exercises Library</h1>
-      </div>
+        <div className='app'>
+          <h1>BARWIS Exercise Library</h1>
+          <EventDashboard/>
+        </div>
     );
   }
 }
