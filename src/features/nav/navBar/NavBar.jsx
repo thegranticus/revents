0760a03833507c9a45880e41822700694468c19d
thegranticus <gc@thegranticus.com>
2018-07-18 18:45:53 -0400

    End of Section 15

--- src/features/nav/navBar/NavBar.jsx
+++ src/features/nav/navBar/NavBar.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { withFirebase } from 'react-redux-firebase'
 import { Menu, Container, Button } from 'semantic-ui-react'
 import { NavLink, Link, withRouter } from 'react-router-dom'
 import SignedInMenu from '../NavBar/Menus/SignedInMenu'
 import SignedOutMenu from '../NavBar/Menus/SignedOutMenu'
 import { openModal } from '../../modals/modalActions'
-import { logout } from '../../auth/authActions'
 
 const actions = {
-    openModal,
-    logout
+    openModal
 }
 
 const mapState = (state) => ({
-    auth: state.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
 })
 
 class NavBar extends Component {
@@ -27,13 +27,13 @@ class NavBar extends Component {
     };
 
     handleSignOut = () => {
-        this.props.logout();
+        this.props.firebase.logout();
         this.props.history.push('/');
     };
 
     render() {
-        const { auth } = this.props;
-        const authenticated = auth.authenticated;
+        const { auth, profile } = this.props;
+        const authenticated = auth.isLoaded && !auth.isEmpty;
         return (
             <Menu inverted fixed="top">
                 <Container>
@@ -51,7 +51,7 @@ class NavBar extends Component {
                             <Button as={Link} to='/createEvent' floated="right" positive inverted content="Create +" />
                         </Menu.Item>}
                     {authenticated ? (
-                        <SignedInMenu currentUser={auth.currentUser} signOut={this.handleSignOut} />
+                        <SignedInMenu profile={profile} signOut={this.handleSignOut} />
                      ) : (
                      <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister}/>
                      )}
@@ -61,4 +61,4 @@ class NavBar extends Component {
     }
 }
 
-export default withRouter(connect(mapState, actions)(NavBar));
+export default withRouter(withFirebase(connect(mapState, actions)(NavBar)));
